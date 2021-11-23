0c16531b32b29074b1fde05170c1b26f358a4cc5
anujdarji2001 <anujdarji2001@gmail.com>
2021-11-23 14:18:15 +0530

    First Commit

---
+++ src/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+const Footer=()=>{
+    return(
+        <>
+        <footer className="bg-light text-center">
+            <p>© 2021 Anuj Darji. All rights reserved.</p>
+        </footer>
+        </>
+    );
+
+};
+export default Footer;
---
+++ src/Sdata.jsx
@@ -0,0 +1,60 @@
+import web from "../src/images/Web.jpg";
+import app from "../src/images/Digital.jpg";
+import android from "../src/images/android.jpg";
+import digital from "../src/images/Marketing.jpg";
+import SEO from "../src/images/SEO.jpg";
+import software from "../src/images/Business.webp";
+import django from "../src/images/django.jpg"
+import maxresdefault from "../src/images/maxresdefault.jpg"
+import python from "../src/images/python.jpg"
+
+const Sdata=[
+    {
+        imgsrc:maxresdefault,
+        title:"MERN Development",
+        desc:"We have a team of skilled Software Designer and Development to manage your Website. You'll get thoughtful, effective web designs created by professionals."
+    },
+    {
+        imgsrc:python,
+        title:"Python Development",
+        desc:"We have a team of skilled Software Designer and Development to manage your Website. You'll get thoughtful, effective web designs created by professionals."
+    },
+    {
+        imgsrc:django,
+        title:"DJango Development",
+        desc:"We have a team of skilled Software Designer and Development to manage your Website. You'll get thoughtful, effective web designs created by professionals."
+    },
+    {
+        imgsrc:web,
+        title:"Web Development",
+        desc:"We have a team of skilled Web Designer and Development to manage your website. You'll get thoughtful, effective web designs created by professionals. ",
+    },
+    {
+        imgsrc:app,
+        title:"App Development",
+        desc:"We have a team of skilled App Designer and Development to manage your App. You'll get thoughtful, effective web designs created by professionals."
+    },
+    {
+        imgsrc:android,
+        title:"Android Development",
+        desc:"We have a team of skilled Android Designer and Development to manage your Android App. You'll get thoughtful, effective web designs created by professionals."
+    
+    },
+    {
+        imgsrc:digital,
+        title:"Digital Marketing",
+        desc:"We have a team of skilled digital marketer and Development to manage your App. You'll get thoughtful, effective web designs created by professionals."
+    },
+    {
+        imgsrc:SEO,
+        title:"Search Engine Optimization",
+        desc:"We have a team of skilled Search Engine Optimization managers to manage your Website. You'll get thoughtful, effective web designs created by professionals."
+    },
+    {
+        imgsrc:software,
+        title:"Software Development",
+        desc:"We have a team of skilled Software Designer and Development to manage your Website. You'll get thoughtful, effective web designs created by professionals."
+    },
+];
+
+export default Sdata;
